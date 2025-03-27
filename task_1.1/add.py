68285f794dbc785c9539572a077c0362247c108f
lorondo <mralexanderlindsay@gmail.com>
2025-03-26 17:39:44 -0700

    Added TestScripts folder

---
+++ task_1.1/add.py
@@ -0,0 +1,11 @@
+import math
+
+first_number = int(input("Give me one number: "))
+second_number = int (input("Give me a second number: "))
+
+a = first_number
+b = second_number
+
+c = a + b
+
+print("Those two numbers added together are: ", c)
