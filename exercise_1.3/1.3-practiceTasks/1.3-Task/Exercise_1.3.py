f0156d52521d5b8005e0c487e772b42efa5bb92d
lorondo <mralexanderlindsay@gmail.com>
2025-04-01 21:08:25 -0700

    added task code and screenshots

--- exercise_1.3/1.3-practiceTasks/1.3-Task/Exercise_1.3.py
+++ exercise_1.3/1.3-practiceTasks/1.3-Task/Exercise_1.3.py
@@ -1,4 +1,59 @@
 recipes_list = []
 ingredients_list = []
 
+def take_recipe():
+  name = input("Enter the recipe name: ")
+  cooking_time = int(input("Enter the cooking time (in minutes): "))
+    
+  ingredients_input = input("Enter ingredients separated by commas: ") 
+  ingredients = [ingredient.strip() for ingredient in ingredients_input.split(",")] 
+    
+  recipe = {
+    "name": name,
+    "cooking_time": cooking_time, 
+    "ingredients": ingredients
+  }
+  return recipe
 
+n = int(input("How many recipes would you like to enter: "))
+
+for i in range(n):
+  recipe = take_recipe()
+  
+  for ingredient in recipe["ingredients"]:
+    if ingredient not in ingredients_list:
+      ingredients_list.append(ingredient)
+  
+  recipes_list.append(recipe)
+
+for recipe in recipes_list:
+  num_ingredients = len(recipe["ingredients"])
+  cooking_time = recipe["cooking_time"]
+
+  if cooking_time < 10 and num_ingredients < 4:
+    difficulty = "Easy"
+  elif cooking_time < 10 and num_ingredients >= 4:
+    difficulty = "Medium"
+  elif cooking_time >= 10 and num_ingredients < 4:
+    difficulty = "Intermediate"
+  elif cooking_time >= 10 and num_ingredients >= 4:
+    difficulty = "Hard" 
+
+  recipe["difficulty"] = difficulty
+
+for recipe in recipes_list:
+  print(f"Recipe: <{recipe['name']}")
+  print(f"Cooking Time (min):  <{recipe['cooking_time']}")
+  print(f"Ingredients:")
+  for ingredient in recipe['ingredients']:
+    print(f"{ingredient}")
+  print(f"Difficulty level: <{recipe['difficulty']}")
+  print()
+
+print("Ingredients Available Across All Recipes")
+ingredients_list.sort() 
+for ingredient in ingredients_list:
+  print({ingredient})
+
+
+  
