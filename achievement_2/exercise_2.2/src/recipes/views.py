2e866a2da5c8b240a47f4fccbf51cc1407831ccb
lorondo <mralexanderlindsay@gmail.com>
2025-05-24 21:27:37 -0700

    uploaded changes for 2.6

--- achievement_2/exercise_2.2/src/recipes/views.py
+++ achievement_2/exercise_2.2/src/recipes/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Recipe
+#to protect class-based view
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def home(request):
    return render(request, 'recipes/recipes_home.html')
 
 # Create your views here.
-class RecipeListView(ListView):
+class RecipeListView(LoginRequiredMixin, ListView):
     model = Recipe
     template_name = 'recipes/main.html'
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'recipes/detail.html'
